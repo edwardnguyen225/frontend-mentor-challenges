e02567891894586709d720bb594c3352a4f0879b
Nhan Nguyen - Edward <48123439+edwardnguyen225@users.noreply.github.com>
2023-11-20 00:18:01 +0700

    feat(tabs): First add tabs

--- planets-fact-site/app/page.tsx
+++ planets-fact-site/app/page.tsx
@@ -1,6 +1,72 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Header from "./header";
+import clsx from "clsx";
+
+enum PlanetInfoType {
+  Overview = "Overview",
+  Structure = "Structure",
+  Surface = "Surface",
+}
+
+const Tab = ({
+  label,
+  isActive,
+  href,
+  onClick,
+}: {
+  label: string;
+  isActive: boolean;
+  href: string;
+  onClick: () => void;
+}) => {
+  return (
+    <li className="me-2">
+      <a
+        href={href}
+        className={clsx([
+          "flex-grow",
+          "uppercase text-[9px] font-bold tracking-widest",
+          "inline-block p-4 border-b-2 rounded-t-lg",
+          isActive && "text-white",
+          isActive ? "border-b-4 border-[#419EBB]" : "border-transparent",
+          "hover:text-white hover:bg-[#38384F]",
+        ])}
+        aria-current={isActive ? "page" : false}
+        onClick={onClick}
+      >
+        {label}
+      </a>
+    </li>
+  );
+};
 
 export default function Home() {
-  return <Header />;
+  const [activeTab, setActiveTab] = useState<PlanetInfoType>(
+    PlanetInfoType.Overview,
+  );
+
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="text-sm font-medium text-center text-white/50 border-b border-white/50">
+          <ul className="flex justify-between">
+            {(Object.keys(PlanetInfoType) as Array<PlanetInfoType>).map(
+              (infoType) => (
+                <Tab
+                  key={infoType}
+                  label={infoType}
+                  isActive={activeTab === infoType}
+                  href="#"
+                  onClick={() => setActiveTab(infoType)}
+                />
+              ),
+            )}
+          </ul>
+        </div>
+      </main>
+    </>
+  );
 }
