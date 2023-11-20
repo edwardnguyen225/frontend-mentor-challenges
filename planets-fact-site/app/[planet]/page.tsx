1a0c00fc727c5b23966f975ce5ff9a9fdaf5883a
Nhan Nguyen - Edward <48123439+edwardnguyen225@users.noreply.github.com>
2023-11-20 16:59:44 +0700

    feat(metadata): Make metadata dynamic

--- planets-fact-site/app/[planet]/page.tsx
+++ planets-fact-site/app/[planet]/page.tsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { Metadata, ResolvingMetadata } from "next";
 import { PlanetInfoPage } from "@components";
+import { PlanetNames } from "../lib/planets";
+
+type Props = {
+  params: { planet: string };
+};
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const planet = params.planet as PlanetNames;
+
+  const capitalizedPlanet = planet.charAt(0).toUpperCase() + planet.slice(1);
+
+  // TODO: Add open graph image
+  return {
+    title: "The Planets Fact - " + capitalizedPlanet,
+  };
+}
 
 export default function Home() {
   return <PlanetInfoPage />;
