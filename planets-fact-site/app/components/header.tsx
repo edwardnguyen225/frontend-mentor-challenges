fdc6e2f2601d41d7c128cb237d18ab930ce8d7e9
Nhan Nguyen - Edward <48123439+edwardnguyen225@users.noreply.github.com>
2023-11-20 13:26:01 +0700

    style(header): Add responsive CSS

--- planets-fact-site/app/components/header.tsx
+++ planets-fact-site/app/components/header.tsx
@@ -13,6 +13,16 @@ type Option = {
   color: string;
 };
 
+/**
+ * color: #FFF;
+font-family: Spartan;
+font-size: 11px;
+font-style: normal;
+font-weight: 700;
+line-height: 25px; /* 227.273% 
+letter-spacing: 1px;
+text-transform: uppercase;
+ */
 const DesktopOption = ({ name, path }: Option) => {
   return (
     <Link
@@ -21,7 +31,7 @@ const DesktopOption = ({ name, path }: Option) => {
         "px-4 py-1",
         "flex justify-center items-center",
         "rounded",
-        "text-sm font-semibold uppercase leading-6 hover:bg-[#38384F]",
+        "text-[11px] font-semibold uppercase leading-[25px] tracking-[1px] hover:bg-[#38384F]",
       ])}
     >
       {name}
@@ -74,13 +84,16 @@ export default function Header() {
     <header>
       <nav
         className={clsx([
-          "mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8",
+          "mx-auto flex max-w-7xl items-center justify-between",
           "border-b border-b-white/20",
+          "px-6 py-4",
+          "md:pt-8 md:pb-5 md:flex md:flex-wrap md:gap-9 md:justify-center",
+          "lg:px-8 lg:pt-5 lg:justify-between",
         ])}
         aria-label="Global"
       >
-        <div className="flex lg:flex-1">{siteName}</div>
-        <div className="flex lg:hidden">
+        <div className="flex md:flex-2 md:flex-shrink-0">{siteName}</div>
+        <div className="flex md:hidden">
           <button
             type="button"
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
@@ -90,7 +103,7 @@ export default function Header() {
             <Bars3Icon className="h-6 w-6" aria-hidden="true" stroke="white" />
           </button>
         </div>
-        <Popover.Group className="hidden lg:flex lg:gap-x-2">
+        <Popover.Group className="hidden md:flex md:text-white/75 lg:gap-x-2">
           {Object.entries(planetsColor).map(([name, color]) => (
             <DesktopOption key={name} name={name} path="#" color={color} />
           ))}
@@ -106,7 +119,7 @@ export default function Header() {
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-[#070724] sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div
             className={clsx([
-              "p-6",
+              "px-6 py-4",
               "flex items-center justify-between",
               "border-b border-b-white/20",
             ])}
