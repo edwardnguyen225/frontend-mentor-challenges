d48bc94fe94a206cb01e00c31288c9d27051ffaf
Nhan Nguyen - Edward <48123439+edwardnguyen225@users.noreply.github.com>
2024-02-02 17:41:22 +0700

    feat: First init project with Svelte

---
+++ rock-paper-scissors-master/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	let name = 'Nhan Nguyen';
+	let githubProfileLink = 'https://github.com/edwardnguyen225';
+</script>
+
+<div class="attribution">
+	Challenge by
+	<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded
+	by <a href={githubProfileLink} target="_blank" referrerpolicy="no-referrer">{name}</a>.
+</div>
+
+<style>
+	.attribution {
+		font-size: 11px;
+		text-align: center;
+	}
+	.attribution a {
+		color: hsl(228, 45%, 44%);
+	}
+</style>
