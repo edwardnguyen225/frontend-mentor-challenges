477704456cb3f448a204077ff40964f7569c5360
Nhan Nguyen - Edward <48123439+edwardnguyen225@users.noreply.github.com>
2024-02-04 13:55:29 +0700

    fix(icon): Fix invalid prop

--- kanban-task-manager/src/components/Icons/IconChevronDown.tsx
+++ kanban-task-manager/src/components/Icons/IconChevronDown.tsx
@@ -6,7 +6,7 @@ const IconChevronDown: React.FC<{ className?: string }> = ({ className }) => {
       xmlns="http://www.w3.org/2000/svg"
       className={className}
     >
-      <path stroke="#635FC7" stroke-width="2" fill="none" d="m1 1 4 4 4-4" />
+      <path stroke="#635FC7" strokeWidth="2" fill="none" d="m1 1 4 4 4-4" />
     </svg>
   );
 };
