1c7ee8cc49076d6b02be7a74e302e1bd014a43bc
Nhan Nguyen - Edward <48123439+edwardnguyen225@users.noreply.github.com>
2024-02-14 09:50:47 +0700

    refactor(footer): Move Footer to components

--- rock-paper-scissors-master/src/routes/+page.svelte
+++ rock-paper-scissors-master/src/routes/+page.svelte
@@ -1,20 +1,5 @@
 <script>
-	let name = 'Nhan Nguyen';
-	let githubProfileLink = 'https://github.com/edwardnguyen225';
+	import Footer from '../components/Footer.svelte';
 </script>
 
-<div class="attribution">
-	Challenge by
-	<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded
-	by <a href={githubProfileLink} target="_blank" referrerpolicy="no-referrer">{name}</a>.
-</div>
-
-<style>
-	.attribution {
-		font-size: 11px;
-		text-align: center;
-	}
-	.attribution a {
-		color: hsl(228, 45%, 44%);
-	}
-</style>
+<Footer />
