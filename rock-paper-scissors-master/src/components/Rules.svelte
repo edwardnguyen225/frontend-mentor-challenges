8803fdfd195dee96eca33685b25466e00091bd06
Nhan Nguyen - Edward <48123439+edwardnguyen225@users.noreply.github.com>
2024-02-22 14:25:18 +0700

    feat(rules): Add rules layout for screen width above 1366px

--- rock-paper-scissors-master/src/components/Rules.svelte
+++ rock-paper-scissors-master/src/components/Rules.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-	import { fly } from 'svelte/transition';
+	import { browser } from '$app/environment';
+
+	import { fly, fade } from 'svelte/transition';
 	import { rulesOverlay, gamePlayType } from '$lib/store';
-	import { onDestroy } from 'svelte';
+	import { onDestroy, onMount } from 'svelte';
 
 	let isNormalGame;
 	const unsubscribe = gamePlayType.subscribe((value) => {
@@ -11,20 +13,49 @@
 	const imageSrc = isNormalGame ? 'images/image-rules.svg' : 'images/image-rules-bonus.svg';
 
 	onDestroy(unsubscribe);
+
+	const onEscapeKeyPressed = (e: KeyboardEvent) => {
+		if (e.key === 'Escape') {
+			rulesOverlay.close();
+		}
+	};
+
+	if (browser) {
+		document.addEventListener('keydown', onEscapeKeyPressed);
+
+		onDestroy(() => {
+			document.removeEventListener('keydown', onEscapeKeyPressed);
+		});
+	}
 </script>
 
 {#if $rulesOverlay}
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
 	<div
-		transition:fly={{ y: '-100vh', duration: 500 }}
-		class={'fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50'}
+		class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
+		on:click={rulesOverlay.close}
 	>
-		<div class="bg-white p-8 pt-24 rounded-lg w-screen h-screen flex flex-col items-center gap-28">
-			<h2 class="text-[32px] leading-8 font-semibold text-[var(--dark-text)] uppercase">Rules</h2>
+		<div
+			in:fly={{ y: '-100vh', duration: 500 }}
+			class="bg-white p-8 pt-24 rounded-lg w-screen h-screen flex flex-col items-center gap-28 xl:w-[400px] xl:h-[unset] xl:pt-8 xl:gap-12"
+		>
+			<div class="flex w-full flex-1 justify-center items-center xl:justify-between">
+				<h2 class="text-[32px] leading-8 font-semibold text-[var(--dark-text)] uppercase">Rules</h2>
+
+				<button
+					class="hidden w-12 h-12 justify-center items-center rounded-full xl:flex"
+					tabindex="0"
+					on:click={rulesOverlay.close}
+				>
+					<img src="images/icon-close.svg" alt="Close" />
+				</button>
+			</div>
 
 			<img src={imageSrc} alt="Rules" />
 
 			<button
-				class="close-rules-btn w-12 h-12 flex justify-center items-center rounded-full"
+				class="close-rules-btn w-12 h-12 flex justify-center items-center rounded-full xl:hidden"
 				on:click={rulesOverlay.close}
 			>
 				<img src="images/icon-close.svg" alt="Close" />
